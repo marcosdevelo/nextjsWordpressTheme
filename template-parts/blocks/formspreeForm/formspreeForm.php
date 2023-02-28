718483232263783fe2945735ab188e2bf654eb89
Marcos Gomez <51391521+marcosdevelo@users.noreply.github.com>
2023-02-28 13:37:06 -0500

    Add files via upload

---
+++ template-parts/blocks/formspreeForm/formspreeForm.php
@@ -0,0 +1,8 @@
+<?php 
+$formId = get_field('form_id');
+?>
+
+<div>
+    <h4>formspree form</h4>
+    <div>check your form submissions <a target="_blank" href="https://formspree.io/forms/<?php echo $formId;?>/submissions">here</a>
+</div>
---
+++ template-parts/blocks/propertySearch/propertySearch.php
@@ -0,0 +1,6 @@
+<div>
+    <strong>Property search</strong>
+<div>
+this block no configuration. bla klaljasl jklas ljkasdlkfjldf
+</div>
+</div>
